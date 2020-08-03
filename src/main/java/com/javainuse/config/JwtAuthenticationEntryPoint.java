719f52521b3f1990cb612d62b74afee560a4099d
nguyentronghieu0611 <62282782+nguyentronghieu0611@users.noreply.github.com>
2020-08-03 20:10:25 +0700

    Update JwtAuthenticationEntryPoint.java

--- src/main/java/com/javainuse/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/javainuse/config/JwtAuthenticationEntryPoint.java
@@ -6,9 +6,14 @@ import java.io.Serializable;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.javainuse.model.Response;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
@@ -18,7 +23,9 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException {
-
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Chưa xác thực");
+		Gson gson = new Gson();
+		response.setContentType("application/json");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getOutputStream().println(gson.toJson(new Response("Xác thực không thành công!",1)));
 	}
 }
